caf6b45c7936518069d90f9432882f5c17c167e6
Leonard Laput <online.lorno@gmail.com>
2022-03-21 23:21:13 +0800

    chore: ignore cdk.out files

--- cdk.out/asset.652f723b702edb0bc9ba4b5bfcc4fc610e3a15b6a7053da592cffff3badd3d9f/hello.ts
+++
@@ -1,8 +0,0 @@
-export const handler: any = async (event: any) => {
-  console.log("request:", JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Good night, CDK! You've hit ${event.path}\n`,
-  };
-};
