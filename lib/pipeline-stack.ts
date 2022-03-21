5e7926ea8ef42a7e50290259ec51b67c1658af32
Leonard Laput <online.lorno@gmail.com>
2022-03-21 23:29:11 +0800

    feat: add pipeline synth step

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,10 @@
 import * as cdk from "aws-cdk-lib";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import {
+  CodeBuildStep,
+  CodePipeline,
+  CodePipelineSource,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class PipelineStack extends cdk.Stack {
@@ -7,8 +12,19 @@ export class PipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     // Creates a CodeCommit repository called 'cdk-workshop'
-    new codecommit.Repository(this, "cdk-workshop", {
+    const repo = new codecommit.Repository(this, "cdk-workshop", {
       repositoryName: "cdk-workshop",
+    });
+
+    // The basic pipeline declaration. This sets the initial structure
+    // of our pipeline
+    const pipeline = new CodePipeline(this, "cdk-workshop-pipeline", {
+      pipelineName: "cdk-workshop-pipeline",
+      synth: new CodeBuildStep("cdk-workshop-pipeline-syth-step", {
+        input: CodePipelineSource.codeCommit(repo, "master"),
+        installCommands: ["npm install -g aws-cdk"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
   }
 }
